d686b8f359187a5d721f58776c247538e4c21712
Naveen <its2hot@outlook.com>
2016-06-15 03:49:04 +0530

    first commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Naveen Dangeti on 25/03/1938 Saka.
+//  Copyright © 1938 Saka Naveen Dangeti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
