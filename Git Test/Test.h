c87cadf2b154198a82011ead90bee8cd4fa56ac5
Naveen <its2hot@outlook.com>
2016-06-15 10:43:41 +0530

    Added a Test Class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Naveen Dangeti on 25/03/1938 Saka.
+//  Copyright © 1938 Saka Naveen Dangeti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Naveen Dangeti on 25/03/1938 Saka.
+//  Copyright © 1938 Saka Naveen Dangeti. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
